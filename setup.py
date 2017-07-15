7297abee51cae59d9e6e754f6907874c71a58ee1
Zane Bilous <zanebilous@gmail.com>
2017-07-15 11:12:26 -0400

    Add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="imagebot",
+    version="1.1.0",
+    description="Subreddit image downloader.",
+    author="Zane Bilous",
+    author_email="zanebilous@gmail.com",
+    platforms=["any"],
+    license="MIT",
+    url="https://github.com/Zane-/imagebot",
+    py_modules=['imagebot'],
+    keywords='bot reddit images',
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+)
