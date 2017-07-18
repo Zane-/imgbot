e47db2cd69ec9ff93a4de1bb5edf529722a13116
Zane Bilous <zanebilous@gmail.com>
2017-07-17 23:42:42 -0400

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 6):
 
 setup(
     name="imgbot",
-    version="1.2.0",
+    version="1.2.1",
     description="Subreddit image downloader.",
     author="Zane Bilous",
     author_email="zanebilous@gmail.com",
